5206d94e049bcc3ac38ef93f29e648015b45cd70
yshibata <yasaimasimas85@yahoo.co.jp>
2020-11-22 04:06:41 +0000

    add_goal_feasibility_check

--- FCND-Motion-Planning/motion_planning.py
+++ FCND-Motion-Planning/motion_planning.py
@@ -164,14 +164,20 @@ class MotionPlanning(Drone):
         grid_start = (int(_local_position[0]-north_offset), int(_local_position[1]-east_offset))
         # Set goal as some arbitrary position on the grid
         #grid_goal = (north_offset + 75, -east_offset + 130)
-        grid_goal =   (grid_start[0],grid_start[1]+8)
+        grid_goal =   (grid_start[0]+100,grid_start[1]+100)
+        while(1):
+          if grid[grid_goal[0],grid_goal[1]]==0:
+            break
+          else:  
+            grid_goal = (grid_goal[0] -1 , grid_goal[1]-1)
+	
         # TODO: adapt to set goal as latitude / longitude position and convert
         #grid_goal = (-east_offset + 10, north_offset + 10)
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
-        path, _ = a_star(grid, heuristic, grid_start, grid_goal)
+        path, _ = _a_star(grid, heuristic, grid_start, grid_goal)
         path = prune_pathpoints(path)
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
